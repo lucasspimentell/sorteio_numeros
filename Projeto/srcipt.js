963b0674e5c3b5361948e7464bb8f3277f3a4728
Lucas pimentel <lucas47b@hotmail.com>
2026-05-26 18:40:56 -0300

    animação emoji

--- Projeto/srcipt.js
+++ Projeto/srcipt.js
@@ -19,6 +19,10 @@ botao.addEventListener("click",()=>{
    numero.innerHTML="⏳"
 
    setTimeout(()=>{
+    numero.innerHTML="⌛"
+},2000);
+
+   setTimeout(()=>{
     sorteio(0,1,2,3,4,5,6,7,8,9)
 },3000);
 
